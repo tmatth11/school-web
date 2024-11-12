358a3129c276a023af29fe35b50ef76fd133b5d0
tmatth11 <tmmatthews11@gmail.com>
2024-11-11 23:13:56 -0600

    Create login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+
+import StudentTable from './StudentTable';
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,15 +1,58 @@
+/*
+    Name:
+    Tony Matthews
+
+    Course:
+    CMP SCI 4010-001
+
+    Date:
+    11/11/24
+
+    Description:
+    This file contains the App component which is the root component of the application.
+*/
+
+// Default imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import StudentTable from './components/StudentTable';
+// Material UI imports
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+// Login component import
+import Login from './components/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Create a new QueryClient
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StudentTable />
+    {/* <StudentTable /> */}
+
+    {/* Copy layout from Chapter 16 notes */}
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            School-Web
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <QueryClientProvider client={queryClient}>
+        <Login />
+      </QueryClientProvider>
+    </Container>
   </React.StrictMode>
 );
 
